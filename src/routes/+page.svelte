d75c98770a8ceb191570e89c08998cade938654b
dhnpx <122553352+dhnpx@users.noreply.github.com>
2024-02-23 18:04:12 -0800

    added html for bio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let experience = [
+        {
+            "date":"",
+            "company":"",
+            "title":"",
+            "skills": [],
+        }
+    ];
+</script>
+
+<h1>HyungJoo Yoon</h1>
+<div class="bio">
+    <div class="intro">
+        <div>Software Developer</div>
+        <p>I build and break software.</p>
+    </div>
+    <div class="about">
+        I started programming in 2021 in a bootcamp and I never looked back. I
+        got started in web development, but I started dabbling in cyber
+        security, and linux not too long after. Now, I'm a student at Cal State
+        Fullerton to learn more about lower level systems.
+    </div>
+</div>
+<div class="experience"></div>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
