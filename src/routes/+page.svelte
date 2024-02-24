a444598c982f8431648fae5ddd11b1d1bcdff5d0
ulhat <142067280+ulhat@users.noreply.github.com>
2024-02-24 00:42:43 -0800

    added experience

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,30 @@
 <script>
     let experience = [
         {
-            "date":"",
-            "company":"",
-            "title":"",
-            "skills": [],
-        }
+            date: "2022 - 2023",
+            company: "Revature",
+            title: "Associate",
+            desc: "I worked with clients to develop and maintain full-stack web applications. I used Java, SpringBoot, Typesscript, and Angular primarily. I also worked with SQL and NoSQL databases as well as AWS deployments.",
+        },
+        {
+            date: "2020",
+            company: "CJ Logistics",
+            title: "Customer I",
+            desc: "Interfaced with carriers and the warehouse to make sure shipments were correct and on time. Signed off on inound and outboud BOLs. Managed Amazon foods accounts and processed orders and shipments.",
+        },
+    ];
+
+    let projects = [
+        {
+            title: "",
+        },
     ];
 </script>
 
 <h1>HyungJoo Yoon</h1>
 <div class="bio">
     <div class="intro">
-        <div>Software Developer</div>
+        <h3>Software Developer</h3>
         <p>I build and break software.</p>
     </div>
     <div class="about">
@@ -22,7 +34,38 @@
         Fullerton to learn more about lower level systems.
     </div>
 </div>
-<div class="experience"></div>
+<div class="experience">
+    <h2>Experience</h2>
+    <div class="job">
+        <h3>Air Force Research Labratories</h3>
+        <span>Intern</span><span>2024 - Present</span>
+        <p>
+            Conduct research and literature review into Cyber Security and
+            attack surfaces of autonomous space systems. Aggreagte literature
+            and devlop a report and publish on the findings.
+        </p>
+    </div>
+    <div class="job">
+        <h3>Revature</h3>
+        <span>Associate</span><span>2022 - 2023</span>
+        <p>
+            I worked with clients to develop and maintain full-stack web
+            applications in Java, SpringBoot, Typescript, and Angular. I also
+            worked with SQL and NoSQL databases as well as AWS deployments. I
+            set up CI/CD pipelines with Jenkins and GitHub.
+        </p>
+    </div>
+    <div class="job">
+        <h3>CJ Logistics</h3>
+        <span>Customer I</span><span>2020</span>
+        <p>
+            Interfaced with carriers and the warehouse to make sure shipments
+            were correct and on time. Signed off on inbound and outbound BOLs
+            and confirm shipments. Managed Amazon foods accounts and processed
+            orders and shipments.
+        </p>
+    </div>
+</div>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
